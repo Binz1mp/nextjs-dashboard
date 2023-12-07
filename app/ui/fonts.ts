ecf21bac4d391c702f7e9d38709a8eb341b671d3
Binz1mp <binieon@naver.com>
2023-12-07 16:03:30 +0900

    first commit for Chapter 6

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Lusitana, Noto_Sans } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'], });
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+export const notoSans = Noto_Sans({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+})
